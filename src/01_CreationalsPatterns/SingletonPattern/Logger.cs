29334c77893d009915d631ea26ecdea7d15df506
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-18 10:10:52 +0100

    Dodanie ApplicationContext

--- src/01_CreationalsPatterns/SingletonPattern/Logger.cs
+++ src/01_CreationalsPatterns/SingletonPattern/Logger.cs
@@ -3,6 +3,28 @@ using System.IO;
 
 namespace SingletonPattern
 {
+    public class ApplicationContext
+    {
+        public string LoggedUser { get; set; }
+        public DateTime RunningFrom { get; set; }
+
+        private static ApplicationContext _instance;
+
+        public static ApplicationContext Instance
+        {
+            get
+            {
+                if ( _instance == null )
+                    _instance = new ApplicationContext();
+
+                return _instance;
+            }
+        }
+
+        protected ApplicationContext() { }
+
+    }
+
     public class Logger
     {
         private readonly string path = "log.txt";
