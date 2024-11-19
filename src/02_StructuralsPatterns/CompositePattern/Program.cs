c716844f2b161f2f98064bd170c7c637df304ff1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-19 13:02:48 +0100

    Dodanie zastosowanie wzorca composite do drzewa decyzyjnego

--- src/02_StructuralsPatterns/CompositePattern/Program.cs
+++ src/02_StructuralsPatterns/CompositePattern/Program.cs
@@ -3,31 +3,50 @@ namespace CompositePattern;
 class Program
 {
     static void Main(string[] args)
-    {       
-        Console.Write("Are you developer?");       
+    {
+        // BuildingTest();
 
-        if (Response) {
+        FormTest();
 
-            Console.Write("Do you know C#?");
-
-            if (Response)
-            {
-                Console.WriteLine("Welcome on Design Pattern in C# Course!");
-            }
-            else
-            {
-                Console.WriteLine("The Course is not for you.");
-            }
-
-        }
-        else
-        {
-            Console.WriteLine("Have a nice day.");
-        }
-
+        Console.ReadLine();
 
     }
 
-    public static bool Response => Console.ReadKey().Key == ConsoleKey.Y;
+    private static void BuildingTest()
+    {
+        var floor = new Floor("First floor");
+
+        var building = new Building("My House");
+        building.Add(floor);
+
+        var livingRoom = new Room("Living room");
+        var bedRoom = new Room("Bed room");
+
+        floor.Add(livingRoom);
+        floor.Add(bedRoom);
+
+        var table = new Furniture("Table");
+        var table2 = new Furniture("Table 2");
+        livingRoom.Add(table);
+        livingRoom.Add(table2);
+
+
+
+        building.Display();
+    }
+
+    private static void FormTest()
+    {
+        IQuestion questionNotDeveloper = new Decision("Have a nice day.");
+
+        IQuestion questionDP = new Decision("Welcome on Design Pattern in C# Course!");
+        IQuestion questionNotDP = new Decision("The Course is not for you.");
+
+        IQuestion questionCsharp = new Question("Do you know C#?", questionDP, questionNotDP);
+        IQuestion questionDeveloper = new Question("Are you developer?", questionCsharp, questionNotDeveloper);
+
+        questionDeveloper.Ask();
+
+    }
 
 }
---
+++ src/02_StructuralsPatterns/CompositePattern/Question.cs
@@ -0,0 +1,58 @@
+﻿namespace CompositePattern;
+
+interface IQuestion
+{
+    void Ask();
+}
+
+// Node
+class Question : IQuestion
+{
+    public string Prompt { get; set; }
+
+    private IQuestion _positiveAction;
+    private IQuestion _negativeAction;
+
+    public Question(string prompt, IQuestion positiveAction, IQuestion negativeAction)
+    {
+        Prompt = prompt;
+        _positiveAction = positiveAction;
+        _negativeAction = negativeAction;
+    }
+
+    public void Ask()
+    {
+        Console.Write(Prompt);
+
+        if (Response)
+        {
+            _positiveAction?.Ask();
+        }
+        else
+        {
+            _negativeAction?.Ask();
+        }
+    }
+
+    public static bool Response => Console.ReadKey().Key == ConsoleKey.Y;
+}
+
+// Leaf
+class Decision : IQuestion
+{
+    public string Prompt { get; set; }
+
+    public Decision(string prompt)
+    {
+        Prompt = prompt;
+    }
+
+    public void Ask()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(Prompt);
+        Console.ResetColor();
+    }
+}
+
+
