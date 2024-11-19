5952654714a25ad273883db5baa3bc49b6efa113
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-19 14:51:53 +0100

    Dodanie Lazy Proxy

--- src/02_StructuralsPatterns/ProxyPattern/CacheProductRepository.cs
+++ src/02_StructuralsPatterns/ProxyPattern/CacheProductRepository.cs
@@ -1,51 +1,128 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
+using System.Linq.Expressions;
 
-namespace ProxyPattern
+namespace ProxyPattern;
+
+public class DbSet<T> : IQueryable<T>
 {
-    // Proxy (Pośrednik)
-    public class CacheProductRepository : IProductRepository
+    public Type ElementType => throw new NotImplementedException();
+
+    public Expression Expression => throw new NotImplementedException();
+
+    public IQueryProvider Provider => throw new NotImplementedException();
+
+    public IEnumerator<T> GetEnumerator()
     {
-        // Real Subject
-        private IProductRepository _repository;
+        throw new NotImplementedException();
+    }
 
-        private IDictionary<int, Product> products;
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
 
-        public CacheProductRepository(IProductRepository productRepository)
+public class ApplicationDbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+}
+
+public class DbCustomerRepository
+{
+    private readonly ApplicationDbContext context;
+
+    public DbCustomerRepository(ApplicationDbContext context)
+    {
+        this.context = context;
+    }
+
+    public Customer Get(int id)
+    {
+        Customer customer = context.Customers.First();
+
+        // Lazy Loading
+        Console.WriteLine(customer.HomeAddress.City);
+
+        return customer;
+    }
+}
+
+// Proxy (wariant klasowy)
+public class CustomerProxy : Customer
+{
+    public override Address HomeAddress 
+    { 
+        get => base.HomeAddress; 
+
+        set => base.HomeAddress = value; 
+    }
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public virtual Address HomeAddress { get; set; }
+}
+
+public class Address
+{
+    public string City { get; set; }
+    public string Street { get; set; }
+}
+
+
+// Cache Proxy 
+// Lazy Proxy 
+// Protection Proxy
+// Logging Proxy
+
+// Proxy (Pośrednik) - Cache Proxy
+// Proxy (wariant obiektowy)
+
+public class CacheProductRepository : IProductRepository
+{
+    // Real Subject
+    private IProductRepository _repository;
+
+    private IDictionary<int, Product> products;
+
+    public CacheProductRepository(IProductRepository productRepository)
+    {
+        products = new Dictionary<int, Product>();
+
+        _repository = productRepository;
+    }
+
+    public void Add(Product product)
+    {
+        products.Add(product.Id, product);
+    }
+
+    public Product Get(int id)
+    {
+        if (products.TryGetValue(id, out Product product))
         {
-            products = new Dictionary<int, Product>();
+            product.CacheHit++;
 
-            _repository = productRepository;
+            return product;
         }
-
-        public void Add(Product product)
+        else
         {
-            products.Add(product.Id, product);
-        }
+            product = _repository.Get(id); // Użycie RealSubject
 
-        public Product Get(int id)
-        {
-            if (products.TryGetValue(id, out Product product))
+            if (product != null)
             {
-                product.CacheHit++;
-
-                return product;
-            }
-            else
-            {
-                product = _repository.Get(id); // Użycie RealSubject
-
-                if (product != null)
-                {
-                    Add(product);
-                }
-
-                return product;
+                Add(product);
             }
 
+            return product;
         }
 
     }
+
 }
