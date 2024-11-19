501bed1df68311e485cd20342c912f976ca6c349
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-19 12:17:29 +0100

    Dodanie przykładu z budynkiem

---
+++ src/02_StructuralsPatterns/CompositePattern/Room.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern;
+
+public class Building
+{
+    public string Name { get; set; }
+    public Roof Roof { get; set; }
+    public List<Room> Rooms { get; set; }
+    public Floor Floor { get; set; }
+}
+
+public class Room
+{
+    public string Name { get; set; }
+    public List<Furniture> Furnitures { get; set; }
+}
+
+public class Floor
+{
+    public string Name { get; set; }
+}
+
+
+public class Roof
+{
+    public string Name { get; set; }
+}
+
+public class Furniture
+{
+    public string Name { get; set; }
+}
