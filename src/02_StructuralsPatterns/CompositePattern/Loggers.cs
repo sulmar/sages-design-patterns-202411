7f8dc08be4e061ab00a60e12552681c2a66395b1
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-20 08:06:02 +0100

    Dodanie logerów

---
+++ src/02_StructuralsPatterns/CompositePattern/Loggers.cs
@@ -0,0 +1,25 @@
+﻿namespace CompositePattern;
+
+public interface ILogger
+{
+    void Log(string message);
+}
+
+public class Loggers
+{
+    private readonly IEnumerable<ILogger> loggers;
+
+    public Loggers(IEnumerable<ILogger> loggers)
+    {
+        this.loggers = loggers;
+    }
+
+    public void DoSomething()
+    {
+        foreach (var logger in loggers)
+        {
+            logger.Log("DoSomething called");
+        }
+
+    }
+}
