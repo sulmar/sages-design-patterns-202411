6614208df9bbb4b54e1b067302032a536abff668
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-20 11:24:50 +0100

    Dodanie ilości kopii z użyciem wzorca adapter

--- src/02_StructuralsPatterns/LegacyPrinterExercise.UnitTests/LegacyPrinterTests.cs
+++ src/02_StructuralsPatterns/LegacyPrinterExercise.UnitTests/LegacyPrinterTests.cs
@@ -1,7 +1,31 @@
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+
 namespace LegacyPrinterExercise.UnitTests;
 
 public class LegacyPrinterTests
 {
+
+    [Fact]
+    public void PrintDocument_Multi_ShouldCalledMulti()
+    {
+        // Arrange
+        IPrinterAdapter printer = new LegacyPrinterAdapter();
+
+        // Przekierowanie konsoli w celu przetestowania
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+
+        // Act
+        printer.Print("a", 3);
+
+        // Assert
+        var output = stringWriter.ToString();
+        var lineCount = output.Split(Environment.NewLine, StringSplitOptions.None).Length - 1;
+        Assert.Equal(3, lineCount);
+
+
+    }
+
     [Fact]
     public void PrintDocument()
     {
--- src/02_StructuralsPatterns/LegacyPrinterExercise/LegacyPrinter.cs
+++ src/02_StructuralsPatterns/LegacyPrinterExercise/LegacyPrinter.cs
@@ -1,9 +1,56 @@
 ﻿namespace LegacyPrinterExercise;
 
+// Abstract Adapter
+public interface IPrinterAdapter
+{
+    void Print(string document, int copies);
+}
+
+// Concrete Adapter (wariant obiektowy)
+public class LegacyPrinterAdapter : IPrinterAdapter
+{
+    private LegacyPrinter printer;
+
+    public LegacyPrinterAdapter()
+    {
+        printer = new LegacyPrinter();    
+    }
+
+    public void Print(string document, int copies)
+    {
+        for (int copy = 1; copy <= copies; copy++)
+        {
+            printer.PrintDocument(document);
+        }
+    }
+}
+
 public class LegacyPrinter
 {    
     public void PrintDocument(string document)
     {
         Console.WriteLine($"Legacy Printer is printing: {document}");
+    }
+}
+
+public class Printer : IPrinterAdapter
+{
+    decimal _costPerCopy = 0.1m; // Cost of 0.10 zł per copy
+
+    public void Print(string document, int copies = 1)
+    {
+        for (int copy = 1; copy <= copies; copy++)
+        {
+            Console.WriteLine($"Printer is printing: {document}");
+        }
+
+        var cost = CalculateCost(copies);
+        Console.WriteLine($"{cost:C2}");
+    }
+
+    private decimal CalculateCost(int copies)
+    {
+        // Calculate total cost based on the number of copies and cost per copy
+        return copies * _costPerCopy;
     }
 }
