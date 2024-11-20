4712b9e4eb26e2896b24ec9b152d369ab8c1262b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-20 17:17:53 +0100

    Zastosowanie Composite Pattern

---
+++ src/03_BehavioralsPatterns/CommandPattern/Models/CompositeCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace CommandPattern;
+
+public class CompositeCommand : ICommand
+{
+    Queue<ICommand> commands = new Queue<ICommand>();
+
+    public void RegisterCommand(ICommand command)
+    {
+        commands.Enqueue(command);
+    }
+
+    public void Execute()
+    {
+        while (commands.Count > 0)
+        {
+            ICommand command = commands.Dequeue();
+
+            command.Execute();
+        }
+    }
+}
