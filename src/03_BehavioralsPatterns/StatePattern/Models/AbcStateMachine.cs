cb8d0fb54015ef51461afb3f4fd37c3f41e8dbbf
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-21 13:09:42 +0100

    Dodanie relay i message service

--- src/03_BehavioralsPatterns/StatePattern/Models/AbcStateMachine.cs
+++ src/03_BehavioralsPatterns/StatePattern/Models/AbcStateMachine.cs
@@ -3,22 +3,55 @@ using System;
 
 namespace StatePattern.Models.FiniteStateMachine;
 
+public interface IRelayService
+{
+    void Enable(byte id);
+    void Disable(byte id);
+}
+
+public interface IMessageService
+{
+    void Send(string message);
+}
+
+public class FakeRelayService : IRelayService
+{
+    private bool enabled = false;
+    public void Disable(byte id) => enabled = false;
+    public void Enable(byte id) => enabled = true;
+}
+
+public class FakeMessageService : IMessageService
+{
+    public void Send(string message) => Console.WriteLine($"Send by email {message}");
+}
+
 // Strategia
 public class AbcStateMachine : StateMachine<LightSwitchState, LightSwitchTrigger>
 {
-    public AbcStateMachine() : base(LightSwitchState.Off)
+    private readonly IRelayService relayService;
+    private readonly IMessageService messageService;
+
+    public AbcStateMachine(IRelayService relayService, IMessageService messageService) : base(LightSwitchState.Off)
     {
+        this.relayService = relayService;
+        this.messageService = messageService;
+
         this.Configure(LightSwitchState.Off)
-         .OnEntry(() => Console.WriteLine("wyłącz przekaźnik"), "Wyłączprzekaźnik")
-         .Permit(LightSwitchTrigger.Push, LightSwitchState.Medium);
+          .OnEntry(() => relayService.Disable(1))
+          .OnEntry(() => messageService.Send("wyłącz przekaźnik"), "Wyłączprzekaźnik")
+          .Permit(LightSwitchTrigger.Push, LightSwitchState.Medium);
 
         this.Configure(LightSwitchState.Medium)
             .Permit(LightSwitchTrigger.Push, LightSwitchState.On);
 
         this.Configure(LightSwitchState.On)
-            .OnEntry(() => Console.WriteLine("załącz przekaźnik"), "załączprzekaźnik")
+            .OnEntry(() => relayService.Enable(1))
+            .OnEntry(() => messageService.Send("załącz przekaźnik"), "załączprzekaźnik")
             .Permit(LightSwitchTrigger.Push, LightSwitchState.Off);
 
         this.OnTransitioned(t => Console.WriteLine($"{t.Trigger} : {t.Source} -> {t.Destination} "));
+        this.relayService = relayService;
+        this.messageService = messageService;
     }
 }
--- src/03_BehavioralsPatterns/StatePattern/Models/LightSwitchStateMachineFactory.cs
+++ src/03_BehavioralsPatterns/StatePattern/Models/LightSwitchStateMachineFactory.cs
@@ -6,12 +6,21 @@ namespace StatePattern.Models.FiniteStateMachine;
 // Factory
 public class LightSwitchStateMachineFactory
 {
+    private readonly IRelayService relayService;
+    private readonly IMessageService messageService;
+
+    public LightSwitchStateMachineFactory(IRelayService relayService, IMessageService messageService)
+    {
+        this.relayService = relayService;
+        this.messageService = messageService;
+    }
+
     public StateMachine<LightSwitchState, LightSwitchTrigger> Create(string model)
     {
-        switch(model)
+        switch (model)
         {
-            case "abc": return new AbcStateMachine();
-            case "xyz": return new AbcStateMachine();
+            case "abc": return new AbcStateMachine(relayService, messageService);
+            case "xyz": return new AbcStateMachine(relayService, messageService);
 
             default: throw new NotSupportedException();
         }
