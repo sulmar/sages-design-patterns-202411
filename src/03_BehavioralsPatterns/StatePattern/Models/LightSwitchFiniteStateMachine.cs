c57975e4906e0343d8d427b771ce579dc5fc131d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-21 12:29:36 +0100

    Dodanie oryginalnej implementacji

--- src/03_BehavioralsPatterns/StatePattern/Models/LightSwitchFiniteStateMachine.cs
+++ src/03_BehavioralsPatterns/StatePattern/Models/LightSwitchFiniteStateMachine.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.PortableExecutable;
 using System.Text;
@@ -19,7 +20,21 @@ public class LightSwitch
 
     public virtual void Push() {
 
-       
+        if (State == LightSwitchState.Off)
+        {
+            Console.WriteLine("załącz przekaźnik");
+
+            State = LightSwitchState.On;
+            return;
+        }
+
+        if (State == LightSwitchState.On)
+        {
+            Console.WriteLine("wyłącz przekaźnik");
+
+            State = LightSwitchState.Off;
+            return;
+        }
 
     }
 }
